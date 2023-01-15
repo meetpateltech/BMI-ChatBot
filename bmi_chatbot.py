d7fac95c6cc1567f568b8c6e67022c30bfad37c9
Meet Patel <113581962+meetpateltech@users.noreply.github.com>
2023-01-15 14:02:41 +0530

    Update bmi_chatbot.py

--- bmi_chatbot.py
+++ bmi_chatbot.py
@@ -1,1 +1,16 @@
+def bmi_chatbot():
+    print("Welcome to the BMI chatbot!")
+    height = float(input("Please enter your height in meters: "))
+    weight = float(input("Please enter your weight in kilograms: "))
+    bmi = weight / (height ** 2)
+    if bmi < 18.5:
+        print("Your BMI is {:.2f}, which is considered underweight.".format(bmi))
+    elif bmi >= 18.5 and bmi < 25:
+        print("Your BMI is {:.2f}, which is considered healthy.".format(bmi))
+    elif bmi >= 25 and bmi < 30:
+        print("Your BMI is {:.2f}, which is considered overweight.".format(bmi))
+    else:
+        print("Your BMI is {:.2f}, which is considered obese.".format(bmi))
+    print("Thank you for using the BMI chatbot!")
 
+bmi_chatbot()
